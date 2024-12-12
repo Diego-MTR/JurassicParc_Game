d9eb63309571b31981cbe005162023834bd05eef
Tufan95 <kabalitufan24@gmail.com>
2024-12-12 15:36:06 +0100

    reglage bug son

--- game/sounds.py
+++ game/sounds.py
@@ -3,16 +3,18 @@ import pygame
 class SoundManager:
     def __init__(self):
         self.sounds = {
-            'click': pygame.mixer.Sound("Assets/sounds/click.ogg"),
-            'game_over': pygame.mixer.Sound("Assets/sounds/game_over.ogg"),
-            'meteorite': pygame.mixer.Sound("Assets/sounds/meteorite.ogg"),
-            'tir': pygame.mixer.Sound("Assets/sounds/tir.ogg"),
-             'jurassicpark': pygame.mixer.Sound("Assets/sounds/jurassicpark.ogg")
+    'click': pygame.mixer.Sound("Assets/sounds/click.ogg"),
+    'game_over': pygame.mixer.Sound("Assets/sounds/game_over.ogg"),
+    'tir': pygame.mixer.Sound("Assets/sounds/tir.ogg"),
+    'jurassicpark': pygame.mixer.Sound("Assets/sounds/jurassicpark.ogg")
+}
 
-        }
 
     def play(self, name):
         if name == 'jurassicpark':
             self.sounds[name].play(loops=-1)  # Jouer en boucle
         else:
+            self.sounds[name].stop()  # Arrêter le son en cours
             self.sounds[name].play()
+
+
